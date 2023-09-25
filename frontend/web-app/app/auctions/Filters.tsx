ef2182e96fff48f432efb0bf0b1c4f345182ccd2
Sudi <sudisimbadav@gmail.com>
2023-09-25 09:03:26 +0200

    ignore the .deaa file on commit

---
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {
+  pageSize: number;
+  setPageSize: (size: number) => void;
+};
+
+const pageZieButtons = [4, 8, 12];
+
+export default function Filters() {
+  return <div>Filters</div>;
+}
