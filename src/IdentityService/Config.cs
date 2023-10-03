130fb104da555083c852796dc3b880eceaea270c
Sudi <sudisimbadav@gmail.com>
2023-10-03 12:46:48 +0200

    chore: modify identity and search svc

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -36,10 +36,11 @@ public static class Config
                 ClientSecrets = {new Secret("mysecret".Sha256())},
                 AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
                 RequirePkce = false,
-                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                RedirectUris = {"http://localhost:3002/api/auth/callback/id-server"},
                 AllowOfflineAccess = true,
                 AllowedScopes = {"openid", "profile", "bidtoride"},
-                AccessTokenLifetime = 3600 * 24 * 30
+                AccessTokenLifetime = 3600 * 24 * 30,
+                AlwaysIncludeUserClaimsInIdToken = true
             }
         };
 }
